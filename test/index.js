99b4098d19447318aa88826965018f213815c376
wanpan <wanpan96@163.com>
2022-11-18 14:20:33 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,12 @@ import Joystick from '../dist/index.js'
 
 const joystick = new Joystick()
 
-joystick.create({ zone: 'joystick' })
+joystick.create({ zone: 'joystick', backImg: { back: './img/joystick_back.png', front: './img/joystick_frontPressed.png' } })
+
 joystick.on('start', () => {
   console.log('start ===> ')
+})
+
+joystick.on('move', (e, i) => {
+  console.log('move ===> ', i)
 })
