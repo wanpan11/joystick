7c586ff25afe6be16d8a9c16d66af0170248e2e9
wanpan11 <wanpan96@163.com>
2024-07-05 09:10:26 +0000

    Deploying to gh-pages from @ wanpan11/joystick@27d4b96f933c567be48dc11cf61d23d657be71b9 🚀

---
+++ meta__docs.0cc78ff0.async.js
@@ -0,0 +1,97 @@
+"use strict";(self.webpackChunkjoystick_kit=self.webpackChunkjoystick_kit||[]).push([[904],{59089:function(y,e,t){t.r(e),t.d(e,{demos:function(){return I}});var d=t(90228),r=t.n(d),_=t(87999),v=t.n(_),u=t(75271),f=t(22020),I={"docs-demo-customization-demo-0":{component:u.memo(u.lazy(v()(r()().mark(function g(){var k,p;return r()().wrap(function(i){for(;;)switch(i.prev=i.next){case 0:return i.next=2,Promise.resolve().then(t.bind(t,64786));case 2:return k=i.sent,p=k.default,i.abrupt("return",{default:function(){return u.createElement(p,null)}});case 5:case"end":return i.stop()}},g)})))),asset:{type:"BLOCK",id:"docs-demo-customization-demo-0",refAtomIds:[],dependencies:{"index.jsx":{type:"FILE",value:`import Demo from './customization.tsx';
+
+export default () => <Demo />;`},"./customization.tsx":{type:"FILE",value:t(70754).Z}},entry:"index.jsx"},context:{"./customization.tsx":t(64786)},renderOpts:{compile:function(){var g=v()(r()().mark(function p(){var D,i=arguments;return r()().wrap(function(c){for(;;)switch(c.prev=c.next){case 0:return c.next=2,t.e(382).then(t.bind(t,49382));case 2:return c.abrupt("return",(D=c.sent).default.apply(D,i));case 3:case"end":return c.stop()}},p)}));function k(){return g.apply(this,arguments)}return k}()}}}},28661:function(y,e,t){t.r(e),t.d(e,{demos:function(){return I}});var d=t(90228),r=t.n(d),_=t(87999),v=t.n(_),u=t(75271),f=t(27617),I={"docs-demo-dynamic-demo-0":{component:u.memo(u.lazy(v()(r()().mark(function g(){var k,p;return r()().wrap(function(i){for(;;)switch(i.prev=i.next){case 0:return i.next=2,Promise.resolve().then(t.bind(t,39649));case 2:return k=i.sent,p=k.default,i.abrupt("return",{default:function(){return u.createElement(p,null)}});case 5:case"end":return i.stop()}},g)})))),asset:{type:"BLOCK",id:"docs-demo-dynamic-demo-0",refAtomIds:[],dependencies:{"index.jsx":{type:"FILE",value:`import Demo from './dynamic.tsx';
+
+export default () => <Demo />;`},"./dynamic.tsx":{type:"FILE",value:t(85678).Z}},entry:"index.jsx"},context:{"./dynamic.tsx":t(39649)},renderOpts:{compile:function(){var g=v()(r()().mark(function p(){var D,i=arguments;return r()().wrap(function(c){for(;;)switch(c.prev=c.next){case 0:return c.next=2,t.e(382).then(t.bind(t,49382));case 2:return c.abrupt("return",(D=c.sent).default.apply(D,i));case 3:case"end":return c.stop()}},p)}));function k(){return g.apply(this,arguments)}return k}()}}}},99110:function(y,e,t){t.r(e),t.d(e,{demos:function(){return I}});var d=t(90228),r=t.n(d),_=t(87999),v=t.n(_),u=t(75271),f=t(49641),I={"docs-demo-demo-0":{component:u.memo(u.lazy(v()(r()().mark(function g(){var k,p;return r()().wrap(function(i){for(;;)switch(i.prev=i.next){case 0:return i.next=2,Promise.resolve().then(t.bind(t,39428));case 2:return k=i.sent,p=k.default,i.abrupt("return",{default:function(){return u.createElement(p,null)}});case 5:case"end":return i.stop()}},g)})))),asset:{type:"BLOCK",id:"docs-demo-demo-0",refAtomIds:[],dependencies:{"index.jsx":{type:"FILE",value:`import Demo from './index.tsx';
+
+export default () => <Demo />;`},"./index.tsx":{type:"FILE",value:t(59959).Z}},entry:"index.jsx"},context:{"./index.tsx":t(39428)},renderOpts:{compile:function(){var g=v()(r()().mark(function p(){var D,i=arguments;return r()().wrap(function(c){for(;;)switch(c.prev=c.next){case 0:return c.next=2,t.e(382).then(t.bind(t,49382));case 2:return c.abrupt("return",(D=c.sent).default.apply(D,i));case 3:case"end":return c.stop()}},p)}));function k(){return g.apply(this,arguments)}return k}()}}}},10137:function(y,e,t){t.r(e),t.d(e,{demos:function(){return _}});var d=t(75271),r=t(91836),_={}},68015:function(y,e,t){t.r(e),t.d(e,{demos:function(){return _}});var d=t(75271),r=t(56583),_={}},64786:function(y,e,t){t.r(e),t.d(e,{default:function(){return v}});var d=t(79861),r=t(75271),_=t(52676);function v(){return(0,r.useEffect)(function(){var u=new d.Z;u.create({mode:"static",zone:"joystick",size:120,position:{top:"calc(50% - 60px)",left:"calc(50% - 60px)"},backImg:{back:"/img/joystick_back.png",front:"/img/joystick_frontPressed.png"}}),u.on("move",function(f,I){console.log(f),console.log(I)})},[]),(0,_.jsx)("div",{id:"joystick",style:{width:"100%",height:300,background:"#565aff"}})}},39649:function(y,e,t){t.r(e),t.d(e,{default:function(){return v}});var d=t(79861),r=t(75271),_=t(52676);function v(){return(0,r.useEffect)(function(){var u=new d.Z;u.create({mode:"dynamic",zone:"joystick"})},[]),(0,_.jsx)("div",{id:"joystick",style:{width:"100%",height:300,background:"#565aff"}})}},39428:function(y,e,t){t.r(e),t.d(e,{default:function(){return v}});var d=t(79861),r=t(75271),_=t(52676);function v(){return(0,r.useEffect)(function(){var u=new d.Z;u.create({mode:"static",zone:"joystick"})},[]),(0,_.jsx)("div",{id:"joystick",style:{width:"100%",height:300,background:"#565aff"}})}},79861:function(y,e,t){t.d(e,{Z:function(){return W}});var d=t(17069),r=t.n(d),_=t(25298),v=t.n(_),u=t(82092),f=t.n(u),I=function(n,o){var s=o.x-n.x,l=o.y-n.y;return Math.sqrt(s*s+l*l)},g=function(n,o){var s=o.x-n.x,l=o.y-n.y;return Math.atan2(l,s)},k=function(n){return n*180/Math.PI},p=function(n,o){var s={x:0,y:0};return s.x=-(n*Math.cos(o)),s.y=-(n*Math.sin(o)),s},D=function(n){var o="left";return n>45&&n<135?o="up":n>135&&n<225?o="right":n>225&&n<315&&(o="down"),o},i=function(n,o){Object.assign(n.style,o)},C=function(n,o){var s=n.mode,l=n.currentJoystick,a=n.joystickSize,E=n.position,m=n.color,j=n.backImg;s==="static"&&(o.style.position="relative");var P=document.createElement("div"),M=document.createElement("div"),b=document.createElement("div");l.ui=P,l.back=M,l.front=b,P.setAttribute("class","joystick_box"),M.setAttribute("class","back"),b.setAttribute("class","front");var x={position:"absolute",top:s==="static"?E.top:"".concat(l.y-a/2,"px"),left:s==="static"?E.left:"".concat(l.x-a/2,"px"),right:E.right,bottom:E.bottom,opacity:s==="static"?"0.5":"1",transition:"opacity 100ms"},h={width:"".concat(a,"px"),height:"".concat(a,"px"),backgroundColor:m.back!==""?m.back:"red",borderRadius:"100%"},O={width:"".concat(a/2,"px"),height:"".concat(a/2,"px"),backgroundColor:m.front!==""?m.front:"#fff",borderRadius:"100%",position:"absolute",top:"0",left:"0",margin:"".concat(a/4,"px 0 0 ").concat(a/4,"px"),transform:"translate(0px, 0px)"};j.back!==""&&(delete h.backgroundColor,h.backgroundImage="url(".concat(j.back,")"),h.backgroundPosition="center",h.backgroundRepeat="no-repeat",h.backgroundSize="100% 100%"),j.front!==""&&(delete O.backgroundColor,O.backgroundImage="url(".concat(j.front,")"),O.backgroundPosition="center",O.backgroundRepeat="no-repeat",O.backgroundSize="100% 100%"),i(P,x),i(M,h),i(b,O),P.append(M,b),o.appendChild(P),l.build=!0},c="ontouchstart"in window,R={touch:{start:"touchstart",move:"touchmove",end:"touchend,touchcancel"},mouse:{start:"mousedown",move:"mousemove",end:"mouseup"}},L;c?L=R.touch:L=R.mouse;var K=r()(function B(){var n=this;v()(this,B),f()(this,"mode","dynamic"),f()(this,"joystickSize",0),f()(this,"color",{back:"",front:""}),f()(this,"backImg",{back:"",front:""}),f()(this,"position",{top:"100px",left:"100px",right:"",bottom:""}),f()(this,"callBack",{start:void 0,move:void 0,end:void 0}),f()(this,"currentJoystick",{ui:void 0,back:void 0,front:void 0,x:0,y:0,build:!1}),f()(this,"create",function(o){var s=o.mode,l=s===void 0?"dynamic":s,a=o.zone,E=a===void 0?"":a,m=o.size,j=m===void 0?80:m,P=o.color,M=o.backImg,b=o.position,x=b===void 0?{top:"100px",left:"100px",right:"",bottom:""}:b,h=document.getElementById(E);if(h){n.mode=l,n.joystickSize=j,n.position=x,P&&(n.color=P),M&&(n.backImg=M);var O=h;n.mode==="static"&&(C(n,h),O=n.currentJoystick.ui),n.initListener(O)}else throw new Error("zoneNode is empty!")}),f()(this,"initListener",function(o){var s=document.getElementsByTagName("body")[0];o.addEventListener(L.start,function(a){var E,m,j;if((E=a.preventDefault)===null||E===void 0||E.call(a),a.type==="touchstart"){var P=a.changedTouches[0],M=P.clientX,b=P.clientY;n.currentJoystick.x=M,n.currentJoystick.y=b}else{var x=a,h=x.clientX,O=x.clientY;n.currentJoystick.x=h,n.currentJoystick.y=O}n.currentJoystick.build&&n.mode==="dynamic"&&n.currentJoystick.ui.remove(),n.mode==="dynamic"?C(n,o):n.currentJoystick.ui.style.opacity="1",s.addEventListener(L.move,n.move),(m=(j=n.callBack).start)===null||m===void 0||m.call(j,a)});var l=L.end.split(",");l.forEach(function(a){s.addEventListener(a,function(E){var m,j;s.removeEventListener(L.move,n.move),n.destroy(),(m=(j=n.callBack).end)===null||m===void 0||m.call(j,E)})})}),f()(this,"move",function(o){if(n.currentJoystick.build){var s={x:0,y:0};if(o.type==="touchmove"){var l=o.changedTouches[0],a=l.clientX,E=l.clientY;s.x=a,s.y=E}else{var m=o,j=m.clientX,P=m.clientY;s.x=j,s.y=P}var M=n.currentJoystick.back.getBoundingClientRect().width/2,b=I(s,n.currentJoystick),x=g(s,n.currentJoystick),h=k(x),O=Math.min(b,M),T=p(O,x);if(n.currentJoystick.front.style.transform="translate(".concat(T.x,"px,").concat(T.y,"px)"),n.callBack.move){var A=h<0?h+360:h;n.callBack.move(o,{direction:D(A),ang:A})}o.preventDefault()}}),f()(this,"destroy",function(){n.currentJoystick.build&&(n.currentJoystick.front.style.transform="translate(0px, 0px)",n.mode==="dynamic"?(n.currentJoystick.ui.style.opacity="0",setTimeout(function(){n.currentJoystick.build=!1,n.currentJoystick.ui.remove()},100)):n.currentJoystick.ui.style.opacity="0.5")}),f()(this,"on",function(o,s){var l=Object.keys(n.callBack);if(l.includes(o))n.callBack[o]=s;else throw new Error("event type error!")})}),U=K,W=U},73504:function(y,e,t){t.r(e),t.d(e,{texts:function(){return r}});var d=t(22020);const r=[{value:"Custom handle style | \u81EA\u5B9A\u4E49\u624B\u67C4\u6837\u5F0F",paraId:0,tocIndex:0}]},13604:function(y,e,t){t.r(e),t.d(e,{texts:function(){return r}});var d=t(27617);const r=[{value:"Dynamic display position | \u52A8\u6001\u663E\u793A\u4F4D\u7F6E",paraId:0,tocIndex:0}]},13756:function(y,e,t){t.r(e),t.d(e,{texts:function(){return r}});var d=t(49641);const r=[{value:"Fixed position | \u56FA\u5B9A\u4F4D\u7F6E",paraId:0,tocIndex:0}]},59225:function(y,e,t){t.r(e),t.d(e,{texts:function(){return r}});var d=t(91836);const r=[]},27095:function(y,e,t){t.r(e),t.d(e,{texts:function(){return r}});var d=t(56583);const r=[{value:`npm i joystick-kit
+`,paraId:0,tocIndex:1},{value:`import Joystick from 'joystick-kit';
+
+const joystick = new Joystick();
+joystick.create({
+  mode: 'static',
+  zone: 'joystick',
+  size: 120,
+  position: { top: 'calc(50% - 60px)', left: 'calc(50% - 60px)' },
+  backImg: {
+    back: '/img/joystick_back.png',
+    front: '/img/joystick_frontPressed.png',
+  },
+});
+`,paraId:1,tocIndex:2},{value:`type CreateConfig {
+  mode: "dynamic" | "static",
+  zone: string; // Set up the mount node
+  size?: number; // Set the size of the handle
+  color?: { back: string; front: string }; // Set a background color
+  backImg?: { back: string; front: string }; // Set a background image (Takes precedence over color)
+  position?: { // Set the handle positioning
+    top?: string | number;
+    left?: string | number;
+    right?: string | number;
+    bottom?: string | number;
+  };
+}
+`,paraId:2,tocIndex:3},{value:`(type: 'start' | 'move' | 'end', cb: (e: Event, info?: { direction: DirectionType; ang: number }) => void)
+`,paraId:3,tocIndex:4}]},70754:function(y,e){e.Z=`import Joystick from 'joystick-kit';
+import React, { useEffect } from 'react';
+
+export default function Demo() {
+  useEffect(() => {
+    const joystick = new Joystick();
+    joystick.create({
+      mode: 'static',
+      zone: 'joystick',
+      size: 120,
+      position: { top: 'calc(50% - 60px)', left: 'calc(50% - 60px)' },
+      backImg: {
+        back: '/img/joystick_back.png',
+        front: '/img/joystick_frontPressed.png',
+      },
+    });
+
+    joystick.on('move', (e, info) => {
+      console.log(e);
+      console.log(info);
+    });
+  }, []);
+
+  return (
+    <div
+      id="joystick"
+      style={{ width: '100%', height: 300, background: '#565aff' }}
+    />
+  );
+}
+`},85678:function(y,e){e.Z=`import Joystick from 'joystick-kit';
+import React, { useEffect } from 'react';
+
+export default function Demo() {
+  useEffect(() => {
+    const joystick = new Joystick();
+    joystick.create({ mode: 'dynamic', zone: 'joystick' });
+  }, []);
+
+  return (
+    <div
+      id="joystick"
+      style={{ width: '100%', height: 300, background: '#565aff' }}
+    />
+  );
+}
+`},59959:function(y,e){e.Z=`import Joystick from 'joystick-kit';
+import React, { useEffect } from 'react';
+
+export default function Demo() {
+  useEffect(() => {
+    const joystick = new Joystick();
+    joystick.create({ mode: 'static', zone: 'joystick' });
+  }, []);
+
+  return (
+    <div
+      id="joystick"
+      style={{ width: '100%', height: 300, background: '#565aff' }}
+    />
+  );
+}
+`}}]);
